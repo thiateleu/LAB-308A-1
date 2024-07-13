cd15ef0010e31e4a8c086102dd291df1e1db516d
thiateleu <thiateleu@gmail.com>
2024-07-13 12:56:38 -0400

    2 commit

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function stackOverflowExample(counter) {
   }
 }
 stackOverflowExample(0);
+
+///////////////// Part 2: Trampolines ///////////////////////
+function Trampolines(fn) {
+  return function (...args) {
+    let result = fn(...args);
+    while (typeof result === "function") {
+      result = result();
+    }
+    return result;
+  };
+}
+/////////////////// Part 3: Deferred Execution //////////////////////
